39681f0f5b39084a57d513982a471fd91201e898
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-14 18:42:50 +0800

    feat: deal with some writting cases in file layer (#10)

--- crates/log-template/src/file.rs
+++ crates/log-template/src/file.rs
@@ -7,6 +7,14 @@ use tracing_subscriber::fmt::format::{FormatEvent, FormatFields};
 use tracing_subscriber::fmt::{format, FmtContext};
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::{EnvFilter, Layer};
+/// Generate a file log layer for tracing.
+///
+/// # Arguments
+///
+/// - `level`: The desired log level filter to set.
+/// - `filepath`: The Path of log file.
+/// - `overwrite`: whether to Overwrite log file if it is existed.
+///
 /// # Example
 ///
 /// ```
@@ -20,7 +28,7 @@ use tracing_subscriber::{EnvFilter, Layer};
 ///        );
 /// let f = std::path::PathBuf::from(f);
 /// tracing_subscriber::registry()
-///     .with(log_template::file_layer(LevelFilter::TRACE, f))
+///     .with(log_template::file_layer(LevelFilter::TRACE, f, true))
 ///     .init();
 /// trace!("Trace message");
 /// debug!("Debug message");
@@ -31,6 +39,7 @@ use tracing_subscriber::{EnvFilter, Layer};
 pub fn file_layer<S>(
     level: LevelFilter,
     filepath: PathBuf,
+    overwrite: bool,
 ) -> Box<dyn Layer<S> + Send + Sync + 'static>
 where
     S: tracing_core::Subscriber,
@@ -39,7 +48,13 @@ where
     if !filepath.parent().unwrap().exists() {
         std::fs::create_dir_all(filepath.parent().unwrap()).unwrap();
     }
-    let a = std::fs::File::create(filepath).unwrap();
+    let a = std::fs::File::options()
+        .write(!filepath.exists() || overwrite)
+        .append(filepath.exists() && !overwrite)
+        .create(true)
+        .open(filepath)
+        .unwrap();
+
     tracing_subscriber::fmt::layer()
         .event_format(FileFormatter)
         .with_writer(a)
@@ -97,14 +112,11 @@ mod tests {
     use super::*;
     #[test]
     fn test_log() {
-        let f = format!(
-            "./temp/{}.log",
-            chrono::Local::now().format("%Y-%m-%d-%H-%M-%S")
-        );
-
-        let f = std::path::PathBuf::from(f);
+        let f1 = std::path::PathBuf::from("./temp/a.log");
+        let f2 = std::path::PathBuf::from("./temp/b.log");
         tracing_subscriber::registry()
-            .with(file_layer(LevelFilter::TRACE, f))
+            .with(file_layer(LevelFilter::TRACE, f1, true))
+            .with(file_layer(LevelFilter::TRACE, f2, false))
             .init();
         trace!("Trace message");
         debug!("Debug message");

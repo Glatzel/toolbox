f502e1bd3c7b06641fb7f304a1a4659a4a947eb4
Glatzel <893016099@qq.com>
2025-03-09 10:50:55 +0800

    fix

--- src/macros.rs
+++ src/macros.rs
@@ -1,14 +1,63 @@
-
-#[cfg(feature = "tracing")]
+#[cfg(feature = "log")]
+pub use tracing;
+#[cfg(feature = "log")]
 #[macro_export]
-macro_rules! trace_log {
+macro_rules! trace {
     ($($arg:tt)*) => {
-        tracing::info!($($arg)*);
+        $crate::tracing::trace!($($arg)*);
+    };
+}
+#[cfg(feature = "log")]
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+       $crate::tracing::debug!($($arg)*);
+    };
+}
+#[cfg(feature = "log")]
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+       $crate:: tracing::info!($($arg)*);
+    };
+}
+#[cfg(feature = "log")]
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        $crate::tracing::warn!($($arg)*);
+    };
+}
+#[cfg(feature = "log")]
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+       $crate:: tracing::error!($($arg)*);
     };
 }
 
-#[cfg(not(feature = "tracing"))]
+#[cfg(not(feature = "log"))]
 #[macro_export]
-macro_rules! trace_log {
+macro_rules! trace {
+    ($($arg:tt)*) => {};
+}
+#[cfg(not(feature = "log"))]
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {};
+}
+#[cfg(not(feature = "log"))]
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {};
+}
+#[cfg(not(feature = "log"))]
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {};
+}
+#[cfg(not(feature = "log"))]
+#[macro_export]
+macro_rules! error {
     ($($arg:tt)*) => {};
 }

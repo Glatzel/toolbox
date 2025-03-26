e7a2704e18396a8f3c0d774a8e34900b5543e2da
Glatzel <893016099@qq.com>
2025-03-26 13:41:52 +0800

    bump 0.0.5

--- scripts/update-version.ps1
+++ scripts/update-version.ps1
@@ -1,11 +1,6 @@
 Set-Location $PSScriptRoot/..
 $version= "0.0.5"
 
-# Update python version
-$cargoTomlPath = "./python/Cargo.toml"
-(Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
-Write-Host "Updated python version to $version"
-
 # Update the version in Cargo.toml
 $cargoTomlPath = "./rust/Cargo.toml"
 (Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath

ccfb169d4e365583aa5813ff61db24ce88c266a7
Glatzel <893016099@qq.com>
2025-03-26 13:34:47 +0800

    fix

--- python/rattler/ci-clerk-py.ps1
+++ python/rattler/ci-clerk-py.ps1
@@ -6,7 +6,6 @@ $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot
 
 & "$PSScriptRoot/../scripts/pytest.ps1"
-& "$PSScriptRoot/../scripts/build-python-whl.ps1" -config $config
 
 Set-Location $PSScriptRoot
 pixi run rattler-build build

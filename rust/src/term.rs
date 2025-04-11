d2e49084ddcebfc067a9ac1577f6f7637ba4edf2
Glatzel <893016099@qq.com>
2025-04-11 12:43:04 +0800

    fix

--- rust/src/term.rs
+++ rust/src/term.rs
@@ -47,7 +47,7 @@ where
 mod tests {
     use tracing::{debug, error, info, trace, warn};
     use tracing_core::LevelFilter;
-    use tracing_subscriber::EnvFilter;
+    
     use tracing_subscriber::layer::SubscriberExt;
     use tracing_subscriber::util::SubscriberInitExt;
 

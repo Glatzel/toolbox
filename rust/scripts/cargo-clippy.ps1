640909aca160103d0959e441792459dce18d3b31
Glatzel <893016099@qq.com>
2025-05-08 01:49:54 +0800

    Update cargo-clippy.ps1

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -1,7 +1,7 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 if ($env:CI) {
-    cargo clippy --all-targets --all-features
+    cargo +stable clippy --all-targets --all-features
 }
 else {
     cargo clippy --fix --all-targets --all-features

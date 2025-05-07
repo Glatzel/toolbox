a5e36deecb5d57524870f3b1f90c35f3c5dc62c2
Glatzel <893016099@qq.com>
2025-05-07 23:28:58 +0800

    fix

--- rust/scripts/cargo-fmt.ps1
+++ rust/scripts/cargo-fmt.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot/..
-# $result = cargo +nightly fmt --all  -- --check
+$result = cargo +nightly fmt --all  -- --check
 
-# if ($result) {
+if ($result) {
     cargo +nightly fmt --all
-    # exit 1
-# }
+    exit 1
+}
 
 Set-Location $ROOT

438b2772294d69cac57a6f83192e356d46843a6f
Glatzel <chenzelin9794@gmail.com>
2025-05-30 04:47:12 +0000

    chore: update configuration files synced local './rust/scripts/cargo-clippy.ps1' with remote './rust/scripts/cargo-clippy.ps1'

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
-cargo +stable clippy --fix --all
+cargo +stable clippy --fix --no-deps
 cargo +stable clippy -- -Dwarnings
 Set-Location $ROOT

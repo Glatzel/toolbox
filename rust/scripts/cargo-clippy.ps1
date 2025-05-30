b33f85c8d649f10aa83682a8f0df822fb0408c24
Glatzel <893016099@qq.com>
2025-05-30 13:18:47 +0800

    chore: update configuration files synced file(s) with Glatzel/template (#89)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local <code>./rust/scripts/cargo-clippy.ps1</code> with
    remote <code>./rust/scripts/cargo-clippy.ps1</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#15339769444](https://github.com/Glatzel/template/actions/runs/15339769444)

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -1,9 +1,10 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
+if (Test-Path $PSScriptRoot/setup.ps1) {
+    &$PSScriptRoot/setup.ps1
+}
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
-if (-not $env:CI) {
-    cargo +stable clippy --fix
-}
+cargo +stable clippy --fix
 cargo +stable clippy -- -Dwarnings
 Set-Location $ROOT

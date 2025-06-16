227ae7a76bcd955498f3c6fee2d317bf9ee73f5d
Glatzel <chenzelin9794@gmail.com>
2025-06-16 02:01:45 +0000

    chore: update configuration files synced local './pre-commit/' with remote './pre-commit/'

--- pre-commit/cargo-clippy.ps1
+++ pre-commit/cargo-clippy.ps1
@@ -1,3 +1,5 @@
+# This File is automatically synchronized from https://github.com/Glatzel/template
+
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel

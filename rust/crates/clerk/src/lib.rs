0a062ed189274a1e569ecd91f327bb20f005b726
Glatzel <893016099@qq.com>
2025-07-03 20:43:35 +0800

    feat: add fast init log (#147)

--- rust/crates/clerk/src/lib.rs
+++ rust/crates/clerk/src/lib.rs
@@ -1,13 +1,18 @@
 #[cfg(feature = "log")]
 mod constants;
 #[cfg(feature = "log")]
+mod fast_setup;
+#[cfg(feature = "log")]
 mod file;
 #[cfg(feature = "log")]
 mod formatter;
 #[cfg(feature = "log")]
 mod term;
+
 #[cfg(feature = "log")]
 use constants::*;
+#[cfg(feature = "log")]
+pub use fast_setup::init_log_with_level;
 #[cfg(feature = "log")]
 pub use file::file_layer;
 #[cfg(feature = "log")]
@@ -16,5 +21,7 @@ use formatter::ClerkFormatter;
 pub use term::terminal_layer;
 #[cfg(feature = "log")]
 pub use tracing;
+#[cfg(feature = "log")]
+pub use tracing::level_filters::LevelFilter;
 
 mod macros;

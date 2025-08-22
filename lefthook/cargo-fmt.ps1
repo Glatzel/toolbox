78e97f14214a4e9cc968edae310cc4a8fe6e3cc4
Glatzel <chenzelin9794@gmail.com>
2025-08-22 12:14:49 +0000

    chore: update configuration files synced local './lefthook/' with remote './lefthook/'

--- lefthook/cargo-fmt.ps1
+++ lefthook/cargo-fmt.ps1
@@ -7,6 +7,8 @@ if ($env:CI -and $args) {
 $ROOT = git rev-parse --show-toplevel
 Set-Location $ROOT
 foreach ($file in $args) {
+    if ("$file".Contains("target")){continue}
+    if ("$file".Contains("crate")){continue}
     Set-Location (Split-Path (Resolve-Path $file) -Parent)
     Write-Output "Cargo fmt in: $pwd"
     cargo +nightly fmt --all

2d6d6f3d51875dea1a09d3f3b477474fdb8a434b
Glatzel <chenzelin9794@gmail.com>
2025-08-29 05:30:58 +0000

    chore: update configuration files synced local './lefthook/' with remote './lefthook/'

--- lefthook/cargo-fmt.ps1
+++ lefthook/cargo-fmt.ps1
@@ -1,15 +1,14 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
 if (-not $args) { exit 0 }
-&$PSScriptRoot/setup.ps1
+. $PSScriptRoot/setup.ps1
 if ($env:CI) {
     rustup toolchain install nightly --profile=minimal
     rustup component add rustfmt --toolchain nightly
 }
-$ROOT = git rev-parse --show-toplevel
-Set-Location $ROOT
 foreach ($file in $args) {
     Set-Location (Split-Path (Resolve-Path $file) -Parent)
     Write-Output "Cargo fmt in: $pwd"
     cargo +nightly fmt --all --quiet
+    Set-Location $ROOT
 }
--- lefthook/numpydoc.ps1
+++ lefthook/numpydoc.ps1
@@ -1,6 +1,5 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
 if (-not $args) { exit 0 }
-&$PSScriptRoot/setup.ps1
-Set-Location ..
+. $PSScriptRoot/setup.ps1
 numpydoc lint $args --ignore ES01 EX01 GL08 PR04 RT03 SA01 SA04

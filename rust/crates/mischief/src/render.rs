8c5540ba592a717b0b52fff00ba63b7234a36583
Glatzel <893016099@qq.com>
2025-09-09 01:53:13 +0800

    更新 render.rs

--- rust/crates/mischief/src/render.rs
+++ rust/crates/mischief/src/render.rs
@@ -44,13 +44,13 @@ impl<'a, T> Render<'a, T>
 where
     T: IDiagnostic,
 {
-    pub fn new(diagnostic: &'a T) -> Self {
+    pub fn new(diagnostic: &'a T) -> Self {#[cfg(feature = "fancy")]
         let terminal_config = TerminalConfig::init();
         Self {
-            diagnostic,
-            indent: Indent,
-            shader: Shader,
-            terminal_config,
+            diagnostic,#[cfg(feature = "fancy")]
+            indent: Indent,#[cfg(feature = "fancy")]
+            shader: Shader,#[cfg(feature = "fancy")]
+            terminal_config,#[cfg(feature = "fancy")]
             theme: Theme::default(),
         }
     }

6e834911208a466ca1272db7025e202fae1d9506
Glatzel <893016099@qq.com>
2025-09-09 20:22:09 +0800

    f

--- rust/scripts/cargo-doc.ps1
+++ rust/scripts/cargo-doc.ps1
@@ -1,5 +1,6 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
 $config = if ($args.Count) { $args } else { @('--no-deps', '--workspace', '--all-features') }
+if (Test-Path $PSScriptRoot/setup.ps1) { &$PSScriptRoot/setup.ps1 }
 Set-Location $PSScriptRoot/..
 cargo doc @config

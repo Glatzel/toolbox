9adbe545a298aaa265c1ca62b75e0716b17b29f4
Glatzel <893016099@qq.com>
2025-10-05 12:54:40 +0800

    f

--- rust/crates/clerk/src/lib.rs
+++ rust/crates/clerk/src/lib.rs
@@ -1,8 +1,13 @@
-#![cfg_attr(feature = "embedded", no_std)]
-#[cfg(all(not(feature = "embedded"), feature = "log"))]
+#![cfg_attr(not(feature = "std"), no_std)]
+#[cfg(all(feature = "std", feature = "log"))]
 mod std_log;
-#[cfg(all(not(feature = "embedded"), feature = "log"))]
+#[cfg(all(feature = "std", feature = "log"))]
 pub use std_log::*;
-
+#[cfg(not(feature = "log"))]
 mod macros;
+#[cfg(all(feature = "log", feature = "std"))]
+pub use defmt::{debug, error, info, trace, warn};
+#[cfg(not(feature = "log"))]
 pub use macros::*;
+#[cfg(all(feature = "log", not(feature = "std")))]
+pub use tracing::{debug, error, info, trace, warn};
--- rust/crates/clerk/src/macros.rs
+++ rust/crates/clerk/src/macros.rs
@@ -1,40 +1,20 @@
-
-// Default: tracing backend (std)
-#[cfg(all(feature = "log", not(feature = "embedded")))]
-mod log_backend {
-    pub use tracing::{debug, error, info, trace, warn};
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => {{}};
 }
-
-// Embedded: defmt backend
-#[cfg(all(feature = "log", feature = "embedded"))]
-mod log_backend {
-    pub use defmt::{debug, error, info, trace, warn};
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {{}};
 }
-
-// No log feature: define no-op macros (zero cost)
-#[cfg(not(feature = "log"))]
-mod log_backend {
-    #[macro_export]
-    macro_rules! trace {
-        ($($arg:tt)*) => {{}};
-    }
-    #[macro_export]
-    macro_rules! debug {
-        ($($arg:tt)*) => {{}};
-    }
-    #[macro_export]
-    macro_rules! info {
-        ($($arg:tt)*) => {{}};
-    }
-    #[macro_export]
-    macro_rules! warn {
-        ($($arg:tt)*) => {{}};
-    }
-    #[macro_export]
-    macro_rules! error {
-        ($($arg:tt)*) => {{}};
-    }
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {{}};
 }
-
-// Re-export unified interface
-pub use log_backend::*;
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {{}};
+}
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {{}};
+}

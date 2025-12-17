6fcd49934e344565cd89a8a30f9559fab8aeba2c
Glatzel <893016099@qq.com>
2025-12-17 20:25:39 +0800

    chore: update configuration files synced file(s) with Glatzel/template (#298)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local directory <code>./lefthook/</code> with remote
    directory <code>./lefthook/</code></li><li>synced local
    <code>./lefthook.yaml</code> with remote
    <code>./lefthook.yaml</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#20302566988](https://github.com/Glatzel/template/actions/runs/20302566988)

--- lefthook/cargo-machete.ps1
+++
@@ -1,9 +0,0 @@
-# This File is automatically synchronized from https://github.com/Glatzel/template
-
-. $PSScriptRoot/setup.ps1
-foreach ($file in $args) {
-    Set-Location (Split-Path (Resolve-Path $file) -Parent)
-    Write-Output "Cargo machete in: $pwd"
-    cargo machete
-    Set-Location $ROOT
-}
--- lefthook/numpydoc.ps1
+++
@@ -1,4 +0,0 @@
-# This File is automatically synchronized from https://github.com/Glatzel/template
-
-. $PSScriptRoot/setup.ps1
-numpydoc lint $args --ignore ES01 EX01 GL08 PR04 RT03 SA01 SA04

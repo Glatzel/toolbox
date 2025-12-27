484ab865bd2d2ce5a440264ff3daac5f3738a8b6
Glatzel <893016099@qq.com>
2025-12-27 14:32:30 +0800

    fix: release ci (#310)

--- python/rattler/build-toolbox.ps1
+++
@@ -1,2 +0,0 @@
-$p=Resolve-Path $env:RECIPE_DIR/..
-pip install $p -v
--- python/rattler/ci-toolbox.ps1
+++
@@ -1,5 +0,0 @@
-$ROOT = git rev-parse --show-toplevel
-Set-Location $PSScriptRoot
-Set-Location $PSScriptRoot
-pixi run rattler-build build
-Set-Location $ROOT

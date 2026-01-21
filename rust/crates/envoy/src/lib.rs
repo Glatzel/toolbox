4f317abc43f22ff32692cba844ce1473ba6f3478
Glatzel <893016099@qq.com>
2026-01-21 12:44:15 +0800

    feat: envoy as str (#322)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- rust/crates/envoy/src/lib.rs
+++ rust/crates/envoy/src/lib.rs
@@ -1,11 +1,13 @@
 #![no_std]
 extern crate alloc;
 
+mod ptr_as_str;
 mod ptr_list_to_vec_string;
 mod ptr_to_string;
 mod to_cstring;
 mod to_vec_cstring;
 
+pub use ptr_as_str::*;
 pub use ptr_list_to_vec_string::*;
 pub use ptr_to_string::*;
 pub use to_cstring::*;

564ff441f2438d6af0e6516742ebe44ea34a5dd5
Glatzel <chenzelin9794@gmail.com>
2026-01-31 12:37:20 +0000

    chore: update configuration files synced local './rust' with remote './rust'

--- rust/scripts/cargo-doc.ps1
+++ rust/scripts/cargo-doc.ps1
@@ -1,5 +1,7 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 $config = if ($args.Count) { $args } else { @('--no-deps', '--workspace', '--all-features') }
 if (Test-Path $PSScriptRoot/setup.ps1) { &$PSScriptRoot/setup.ps1 }
 Set-Location $PSScriptRoot/..

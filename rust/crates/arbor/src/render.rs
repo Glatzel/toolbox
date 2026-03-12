6dccb688419479ac3f653452a7575a6e25ab2011
Glatzel <chenzelin9794@gmail.com>
2026-03-12 11:40:42 +0800

    f

--- rust/crates/arbor/src/render.rs
+++ rust/crates/arbor/src/render.rs
@@ -1,5 +1,6 @@
 use alloc::collections::vec_deque::VecDeque;
 use alloc::string::String;
+use alloc::sync::Arc;
 use core::fmt::{self, Display};
 
 use crate::protocol::{IIndent, ITree, Layer, Line};
@@ -17,25 +18,28 @@ where
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "{}", self.indent.get_indent(Layer::Root, Line::First))?;
         writeln!(f, "{}", self.tree.content())?;
-        let mut queue: VecDeque<(&T, Layer, String)> = VecDeque::new();
-        enqueue(&mut queue, self.tree, String::new());
+        let mut queue: VecDeque<(&T, Layer, Arc<String>)> = VecDeque::new();
+        enqueue(&mut queue, self.tree, Arc::new(String::new()));
         while let Some((leaf, layer, s)) = queue.pop_front() {
             f.write_str(&s)?;
             f.write_str(self.indent.get_indent(layer, crate::protocol::Line::First))?;
             f.write_str(leaf.content())?;
             writeln!(f)?;
             if !leaf.leaves().is_empty() {
-                let mut leave_spaces = s.clone();
+                let mut leave_spaces = (*s).clone();
                 leave_spaces.push_str(self.indent.get_indent(layer, Line::Other));
-                enqueue(&mut queue, leaf, leave_spaces);
+                enqueue(&mut queue, leaf, Arc::new(leave_spaces));
             }
         }
         Ok(())
     }
 }
 
-fn enqueue<'a, T>(queue: &mut VecDeque<(&'a T, Layer, String)>, tree: &'a T, spaces: String)
-where
+fn enqueue<'a, T>(
+    queue: &mut VecDeque<(&'a T, Layer, Arc<String>)>,
+    tree: &'a T,
+    spaces: Arc<String>,
+) where
     T: ITree<Leave = T>,
 {
     let children_count_index = tree.leaves().len().saturating_sub(1);

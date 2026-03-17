7ed9420b4834038ffe98c9f75de481408bb43f0c
Glatzel <chenzelin9794@gmail.com>
2026-03-17 11:50:31 +0800

    f

--- rust/crates/mischief/examples/from_std_error.rs
+++ rust/crates/mischief/examples/from_std_error.rs
@@ -5,7 +5,11 @@ use mischief::{IntoMischief, WrapErr};
 fn main() -> mischief::Result<()> {
     let _ = File::open("fake")
         .into_mischief()
-        .wrap_err(mischief::mischief!("mischief wrapper", help = "some help"))
+        .wrap_err(mischief::mischief!(
+            "mischief wrapper",
+            #[cfg(feature = "fancy")]
+            help = "some help"
+        ))
         .wrap_err("error wrapper")?;
     Ok(())
 }

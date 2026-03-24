ad173f165848898e73d77173d09676df813fe1f7
Glatzel <chenzelin9794@gmail.com>
2026-03-24 19:02:27 +0800

    f

--- tools/orc/tests/test.rs
+++ tools/orc/tests/test.rs
@@ -11,9 +11,21 @@ fn test_file() -> String {
         .to_slash_lossy()
         .to_string()
 }
+fn path_env() -> String {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join(".pixi")
+        .join("envs")
+        .join("default")
+        .join("Library")
+        .join("bin")
+        .to_slash_lossy()
+        .to_string()
+}
 #[test]
 fn test_simple() {
     let cmd = Command::new(assert_cmd::cargo_bin!("orc"))
+        .env_clear()
+        .env("PATH", path_env())
         .args([test_file()])
         .assert()
         .success();
@@ -25,6 +37,8 @@ fn test_simple() {
 #[case(2)]
 fn test_limit(#[case] limit: usize) {
     let cmd = Command::new(assert_cmd::cargo_bin!("orc"))
+        .env_clear()
+        .env("PATH", path_env())
         .args(["-l", &limit.to_string()])
         .args([test_file()])
         .assert()
@@ -37,6 +51,8 @@ fn test_limit(#[case] limit: usize) {
 #[test]
 fn test_missing() {
     let cmd = Command::new(assert_cmd::cargo_bin!("orc"))
+        .env_clear()
+        .env("PATH", path_env())
         .args(["-l", "0"])
         .args(["-s", "missing"])
         .args([test_file()])

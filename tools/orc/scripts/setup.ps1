8fe635d9d52ccba582dfe437665fdf915d4eaa86
Glatzel <chenzelin9794@gmail.com>
2026-03-25 09:00:39 +0800

    f

--- tools/orc/scripts/setup.ps1
+++ tools/orc/scripts/setup.ps1
@@ -2,3 +2,4 @@ Remove-Item $PSScriptRoot/../.pixi -Recurse -Force -ErrorAction SilentlyContinue
 pixi install
 Remove-Item $PSScriptRoot/../.pixi/envs/default/Library/bin/jpeg8.dll -ErrorAction SilentlyContinue
 Remove-Item $PSScriptRoot/../.pixi/envs/default/Library/bin/api-ms-win*.dll -ErrorAction SilentlyContinue
+Remove-Item $PSScriptRoot/../.pixi/envs/default/api-ms-win-crt-runtime*.dll -ErrorAction SilentlyContinue

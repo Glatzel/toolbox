b33ae236b382459fdc988cfe3d1abe60a0830918
Glatzel <chenzelin9794@gmail.com>
2026-03-31 12:19:37 +0800

    f

--- tools/orc/src/dep_tree.rs
+++ tools/orc/src/dep_tree.rs
@@ -196,7 +196,7 @@ impl DepTree {
                 let binary = match Elf::parse(&buf) {
                     Ok(p) => p,
                     Err(_e) => {
-                        return None;
+                        return ;
                     }
                 };
 
@@ -325,7 +325,7 @@ impl DepTree {
                             let binary = match Elf::parse(&buf) {
                                 Ok(p) => p,
                                 Err(_e) => {
-                                    return None;
+                                    return ;
                                 }
                             };
                             #[cfg(target_os = "windows")]

0691c6be133e300d72732281ac1904b3993caa92
Glatzel <chenzelin9794@gmail.com>
2026-04-12 13:42:30 +0800

    f

--- tools/shook/src/server/job.rs
+++ tools/shook/src/server/job.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use axum::http::HeaderMap;
 use kioyu::IPayload;
-use microsandbox::{ExecEvent, Sandbox};
+use microsandbox::Sandbox;
 use serde::{Deserialize, Serialize};
 use validator::{Validate, ValidationError};
 
@@ -71,21 +71,21 @@ impl IPayload for JobSpec {
                 ],
             )
             .await?;
-        while let Some(event) = handle.recv().await {
-            match event {
-                ExecEvent::Stdout(data) => {
-                    clerk::debug!("{}", String::from_utf8_lossy(&data))
-                }
-                ExecEvent::Stderr(data) => {
-                    clerk::debug!("{}", String::from_utf8_lossy(&data))
-                }
-                ExecEvent::Exited { code } => {
-                    clerk::debug!("Sandbox exited with code: {code}");
-                    break;
-                }
-                _ => {}
-            }
-        }
+        // while let Some(event) = handle.recv().await {
+        //     match event {
+        //         microsandbox::ExecEvent::Stdout(data) => {
+        //             clerk::debug!("{}", String::from_utf8_lossy(&data))
+        //         }
+        //         microsandbox::ExecEvent::Stderr(data) => {
+        //             clerk::debug!("{}", String::from_utf8_lossy(&data))
+        //         }
+        //         microsandbox::ExecEvent::Exited { code } => {
+        //             clerk::debug!("Sandbox exited with code: {code}");
+        //             break;
+        //         }
+        //         _ => {}
+        //     }
+        // }
         clerk::debug!("Sandbox finished: {name}");
         sandbox.stop_and_wait().await?;
         Sandbox::remove(sandbox.name()).await?;

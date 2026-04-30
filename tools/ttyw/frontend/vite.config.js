4bb09f2a523378e800f8d0a62652c43bce41dd37
Glatzel <chenzelin9794@gmail.com>
2026-04-30 17:35:43 +0800

    f

--- tools/ttyw/frontend/vite.config.js
+++ tools/ttyw/frontend/vite.config.js
@@ -6,9 +6,6 @@ export default {
         target: "ws://127.0.0.1:7681",
         ChangeOrigin: true,
         ws: true,
-        configure: (proxy) => {
-          console.log("WS proxy active");
-        },
       },
     },
   },

28f2224fde2b5c48c47ebb32c93a18830d554ab3
Glatzel <893016099@qq.com>
2026-05-01 07:49:42 +0800

    build: add npm package (#487)

--- tools/ttyw/frontend/src/terminal.ts
+++ tools/ttyw/frontend/src/terminal.ts
@@ -1,5 +1,5 @@
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
 import { ClipboardAddon } from "@xterm/addon-clipboard";
 import { WebglAddon } from "@xterm/addon-webgl";
 import { ImageAddon } from "@xterm/addon-image";

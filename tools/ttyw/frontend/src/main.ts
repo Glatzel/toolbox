3fbe5a7c3be0967ff15440920248150218b3ac99
Glatzel <chenzelin9794@gmail.com>
2026-05-01 08:32:14 +0800

    fix: embedded font

--- tools/ttyw/frontend/src/main.ts
+++ tools/ttyw/frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { TerminalClient } from "./terminal.js";
+import "./main.css";
 const term_element = document.getElementById("term");
 if (term_element) {
   new TerminalClient(term_element);

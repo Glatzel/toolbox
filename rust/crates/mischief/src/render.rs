168769f50c9c845ccf70f831a36a13e97ac93204
Glatzel <chenzelin9794@gmail.com>
2026-06-07 13:30:41 +0800

    f

--- rust/crates/mischief/src/render.rs
+++ rust/crates/mischief/src/render.rs
@@ -2,6 +2,7 @@
 mod fancy_render;
 #[cfg(not(feature = "fancy"))]
 mod no_fancy_render;
+#[cfg(feature = "fancy")]
 pub use fancy_render::*;
 #[cfg(not(feature = "fancy"))]
 pub use no_fancy_render::*;

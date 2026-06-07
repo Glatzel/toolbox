1ddca6491357a4e0d1208a5fe5a363cb564f2ab4
Glatzel <893016099@qq.com>
2026-06-07 13:19:12 +0800

    feat(vinaya): remove generate proto cmd (#559)

--- tools/vinaya/crates/vinaya-cli/src/cli/houdini.rs
+++ tools/vinaya/crates/vinaya-cli/src/cli/houdini.rs
@@ -1,4 +1,3 @@
-mod generate_proto;
 mod hfs;
 mod latest;
 mod list;
@@ -11,14 +10,12 @@ pub struct Args {
 }
 #[derive(Debug, Subcommand)]
 pub enum Commands {
-    GenerateProto(generate_proto::Args),
     Hfs(hfs::Args),
     Latest(latest::Args),
     List(list::Args),
 }
 pub fn execute(args: Args) -> mischief::Result<()> {
     match args.command {
-        Commands::GenerateProto(cmd) => generate_proto::execute(cmd),
         Commands::Hfs(cmd) => hfs::execute(cmd),
         Commands::Latest(cmd) => latest::execute(cmd),
         Commands::List(cmd) => list::execute(cmd),

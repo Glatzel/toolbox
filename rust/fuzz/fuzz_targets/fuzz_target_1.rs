4009f81835baefc274f7932bb69fe742bfef2604
Glatzel <chenzelin9794@gmail.com>
2026-07-10 00:11:57 +0800

    f

--- rust/fuzz/fuzz_targets/fuzz_target_1.rs
+++ rust/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -6,16 +6,4 @@ fuzz_target!(|data: &[u8]| {
     if data.len() < 4 {
         return;
     }
-
-    let value = u32::from_le_bytes([
-        data[0],
-        data[1],
-        data[2],
-        data[3],
-    ]);
-
-    // Bug: only crashes for a specific value
-    if value == 0xDEADBEEF {
-        panic!("found magic value");
-    }
 });

a29a976d79596dbc3652ea04a93cb486fb1b551b
Glatzel <chenzelin9794@gmail.com>
2026-07-10 09:33:36 +0800

    f

---
+++ rust-fuzz/fuzz_targets/fuzz_rax.rs
@@ -0,0 +1,20 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use rax::string::IStrFlowRule;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = rax::string::rules::ByteCount::<0>.apply(s);
+        let _ = rax::string::rules::ByteCount::<1>.apply(s);
+        let _ = rax::string::rules::ByteCount::<2>.apply(s);
+
+        let _ = rax::string::rules::Char::<'c'>.apply(s);
+        let _ = rax::string::rules::Char::<'\n'>.apply(s);
+        let _ = rax::string::rules::Char::<'你'>.apply(s);
+
+        let _ = rax::string::rules::CharCount::<0>.apply(s);
+        let _ = rax::string::rules::CharCount::<1>.apply(s);
+        let _ = rax::string::rules::CharCount::<2>.apply(s);
+    }
+});

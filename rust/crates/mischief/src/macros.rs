3edd5c389188b787cf50a9a0f8e859883acf9709
Glatzel <893016099@qq.com>
2026-07-17 09:35:56 +0800

    fix: trailing semicolons in expression macro bodies

--- rust/crates/mischief/src/macros.rs
+++ rust/crates/mischief/src/macros.rs
@@ -15,6 +15,6 @@ pub use mischief_macros::mischief;
 #[macro_export]
 macro_rules! bail {
     ($($arg:tt)*) => {
-        return Err($crate::mischief!($($arg)*));
+        return Err($crate::mischief!($($arg)*))
     };
 }

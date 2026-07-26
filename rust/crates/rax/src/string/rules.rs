0c2ba81570ace6efa43ccb576050c42925c78564
Glatzel <893016099@qq.com>
2026-07-26 18:50:51 +0800

    Update rules.rs

--- rust/crates/rax/src/string/rules.rs
+++ rust/crates/rax/src/string/rules.rs
@@ -40,7 +40,7 @@ pub enum UntilMode {
     KeepInRest,
 }
 impl UntilMode {
-    #[inline(always)]
+    #[inline]
     pub fn split_str(self, input: &str, left: usize, length: usize) -> (&str, &str) {
         unsafe {
             match self {

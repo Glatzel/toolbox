604be206999a18db4d040d2cc7e1a75ac4cda7fb
Glatzel <chenzelin9794@gmail.com>
2026-07-31 16:02:18 +0800

    Update cargo-bench.sh

--- .ci/rust/cargo-bench.sh
+++ .ci/rust/cargo-bench.sh
@@ -17,7 +17,7 @@ fi
 
 if [ "${CI:-}" ]
 then
-    pixi global install -c https://prefix.dev/glatzel cargo-codspeed \
+    pixi global install -c https://prefix.dev/glatzel cargo-codspeed
     cargo codspeed build $CARGO_BENCH_OPTIONS
 else
     cargo bench $CARGO_BENCH_OPTIONS

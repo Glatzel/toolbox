2b4892ebab41283d28aa3d871297dbba3a606e07
Glatzel <chenzelin9794@gmail.com>
2026-08-01 18:01:18 +0800

    Update sidefx.rs

--- tools/vinaya/crates/vinaya-cli/src/cli/sidefx.rs
+++ tools/vinaya/crates/vinaya-cli/src/cli/sidefx.rs
@@ -57,29 +57,23 @@ pub enum Commands {
 pub async fn execute(args: &Args) -> mischief::Result<()> {
     let client_id = args.client_id.as_ref().map_or_else(
         || {
-            env::var("CLIENT_ID").map_or_else(
-                |_| {
+            env::var("CLIENT_ID").unwrap_or_else(|_| {
                     dialoguer::Input::with_theme(&dialoguer::theme::ColorfulTheme::default())
                         .with_prompt("Client ID")
                         .interact_text()
                         .unwrap()
-                },
-                |value| value,
-            )
+                })
         },
         std::clone::Clone::clone,
     );
     let client_secret = args.client_secret.as_ref().map_or_else(
         || {
-            env::var("CLIENT_SECRET").map_or_else(
-                |_| {
+            env::var("CLIENT_SECRET").unwrap_or_else(|_| {
                     dialoguer::Input::with_theme(&dialoguer::theme::ColorfulTheme::default())
                         .with_prompt("Client Secret")
                         .interact_text()
                         .unwrap()
-                },
-                |value| value,
-            )
+                })
         },
         std::clone::Clone::clone,
     );

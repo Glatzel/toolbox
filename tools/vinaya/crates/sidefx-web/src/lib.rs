219db92ede360280607961c4308eeba00aff8c99
Glatzel <chenzelin9794@gmail.com>
2026-08-03 13:12:30 +0800

    Update lib.rs

--- tools/vinaya/crates/sidefx-web/src/lib.rs
+++ tools/vinaya/crates/sidefx-web/src/lib.rs
@@ -71,7 +71,10 @@ impl SideFXWeb {
         let token = response
             .json::<HashMap<String, serde_json::Value>>()
             .await
-            .into_mischief()?["access_token"]
+            .into_mischief()?
+            .get("access_token")
+            .and_then(|v| v.as_str())
+            .ok_or_else(|| mischief::mischief!("No access token found."))?
             .to_string()
             .replace('"', "");
 

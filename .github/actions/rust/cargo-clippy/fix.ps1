47b9f9309cbd617e4cd2136298a4a758c655b6e9
Glatzel <893016099@qq.com>
2026-08-10 09:15:48 +0800

    chore: update shared files synced file(s) with Glatzel/template (#729)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local directory <code>./.github/actions/lefthook/</code> with
    remote directory <code>./.github/actions/lefthook/</code></li><li>synced
    local <code>./.github/workflows/ci-lefthook.yml</code> with remote
    <code>./.github/workflows/ci-lefthook.yml</code></li><li>synced local
    <code>./.github/workflows/changelog.yml</code> with remote
    <code>./.github/workflows/changelog.yml</code></li><li>synced local
    <code>./.github/workflows/release.yml</code> with remote
    <code>./.github/workflows/release.yml</code></li><li>synced local
    directory <code>./.github/actions/rust</code> with remote directory
    <code>./.github/actions/rust</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#31345525191](https://github.com/Glatzel/template/actions/runs/31345525191)

--- .github/actions/rust/cargo-clippy/fix.ps1
+++ .github/actions/rust/cargo-clippy/fix.ps1
@@ -1,4 +1,4 @@
 # This file is automatically synchronized from https://github.com/Glatzel/template
 
 $clippy_args = $(yq '.inputs.args.default' "$PSScriptRoot/action.yml") -split ' '
-cargo +nightly clippy --fix $args -- $clippy_args
+cargo clippy --fix $args -- $clippy_args

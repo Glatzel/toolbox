3eeaae20fba16102db7d3b037efee8c53f009712
Glatzel <chenzelin9794@gmail.com>
2026-08-13 11:51:38 +0800

    f

--- .github/actions/rust/cargo-clippy/fix.ps1
+++
@@ -1,4 +0,0 @@
-# This file is automatically synchronized from https://github.com/Glatzel/template
-
-$clippy_args = $(yq '.inputs.args.default' "$PSScriptRoot/action.yml") -split ' '
-cargo clippy --fix $args -- $clippy_args

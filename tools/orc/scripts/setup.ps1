593f3b4d832b2b48d85c11b987903568fb43c72d
Glatzel <893016099@qq.com>
2026-08-24 10:13:54 +0800

    test(orc): avoid renovate update test file (#774)

--- tools/orc/scripts/setup.ps1
+++ tools/orc/scripts/setup.ps1
@@ -1,5 +1,8 @@
-Remove-Item $PSScriptRoot/../.pixi -Recurse -Force -ErrorAction SilentlyContinue
-pixi install
+rename-item $PSScriptRoot/../_pixi.toml pixi.toml
+rename-item $PSScriptRoot/../_pixi.lock pixi.lock
+pixi reinstall --frozen
+rename-item $PSScriptRoot/../pixi.toml _pixi.toml
+rename-item $PSScriptRoot/../pixi.lock _pixi.lock
 if ($IsWindows) {
     Remove-Item $PSScriptRoot/../.pixi/envs/default/Library/bin/jpeg8.dll -ErrorAction SilentlyContinue
     Remove-Item $PSScriptRoot/../.pixi/envs/default/Library/bin/api-ms-win*.dll -ErrorAction SilentlyContinue

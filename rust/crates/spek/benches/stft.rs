961e6748afca49a8a46cbe02581fe67bb889e733
Glatzel <893016099@qq.com>
2026-09-22 10:20:42 +0800

    Update stft.rs

--- rust/crates/spek/benches/stft.rs
+++ rust/crates/spek/benches/stft.rs
@@ -35,7 +35,7 @@ fn bench_wrapper<T, B>(
     let mut data = (0..10usize.pow(size as u32))
         .map(|i| num!(i))
         .collect::<Vec<_>>();
-    let mut spectrum = stft.stft(&mut data);
+    let spectrum = stft.stft(&mut data);
     g.bench_with_input(
         BenchmarkId::new(format!("{}_stft_{}_10^", name, fft_size), size),
         &size,
@@ -59,7 +59,7 @@ fn bench_wrapper<T, B>(
         &size,
         |b, _| {
             b.iter(|| {
-                stft.stft(&mut data);
+                stft.istft(&mut spectrum.clone());
             })
         },
     );
@@ -68,7 +68,7 @@ fn bench_wrapper<T, B>(
         &size,
         |b, _| {
             b.iter(|| {
-                stft.par_istft(&mut spectrum);
+                stft.par_istft(&mut spectrum.clone());
             })
         },
     );
